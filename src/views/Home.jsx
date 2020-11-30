bf171669c4b60ac5e729e434e31f3cdb46a077a1
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-11-30 16:27:44 +0700

    Card

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import CovidImage from '../assets/image/covid19.png'
 import {} from '../assets/css/style.css'
 import { Image } from 'react-bootstrap'
+import Card from '../components/Cards'
 const Home = () => {
     return (
         <div className="container">
             <div className="centre">
                 <Image src={CovidImage} />
+            </div>
+            <div>
+                <Card/>
             </div>
         </div>
     )
